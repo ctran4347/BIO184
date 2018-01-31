09dfd11c7ed32126c3bd031d7dee5dca79e3cd0f
xAznboyzx <xAznboyszx@gmail.com>
2018-01-31 09:27:07 -0800

    Corrections

--- Homework/Homework_2/tran-christian-writing_multiple_fasta_files.py
+++ Homework/Homework_2/tran-christian-writing_multiple_fasta_files.py
@@ -3,9 +3,9 @@ sequence_header_DEF456="actgatcgacgatcgatcgatcacgact"
 sequence_header_HIJ789="ACTGAC-ACTGT--ACTGTA----CATGTG"
 new_sequence_header_DEF456=sequence_header_DEF456.upper()
 new_sequence_header_HIJ789=sequence_header_HIJ789.replace("-","")
-sequence_header_ABC123_file=open("sequence_ABC123.fasta","w")
+sequence_header_ABC123_file=open("ABC123.fasta","w")
 sequence_header_ABC123_file.write(">Sequence_ABC123\n" + sequence_header_ABC123)
-sequence_header_DEF456_file=open("sequence_DEF456.fasta","w")
+sequence_header_DEF456_file=open("DEF456.fasta","w")
 sequence_header_DEF456_file.write(">Sequence_DEF456\n" + new_sequence_header_DEF456)
-sequence_header_HIJ789_file=open("sequence_HIJ789.fasta","w")
+sequence_header_HIJ789_file=open("HIJ789.fasta","w")
 sequence_header_HIJ789_file.write(">Sequence_HIJ789\n" + new_sequence_header_HIJ789)
--- Homework/Homework_2/tran-christian_writing_a_fasta_file.py
+++ Homework/Homework_2/tran-christian_writing_a_fasta_file.py
@@ -3,7 +3,7 @@ sequence_header_DEF456="actgatcgacgatcgatcgatcacgact"
 sequence_header_HIJ789="ACTGAC-ACTGT--ACTGTA----CATGTG"
 new_sequence_header_DEF456=sequence_header_DEF456.upper()
 new_sequence_header_HIJ789=sequence_header_HIJ789.replace("-","")
-sequence_header_file=open("sequence.fasta","w")
+sequence_header_file=open("three.fasta","w")
 sequence_header_file.write(">Sequence_ABC123\n" + sequence_header_ABC123 + "\n")
-sequence_header_file.write("\n" + ">Sequence_DEF456\n" + new_sequence_header_DEF456 + "\n")
-sequence_header_file.write("\n" + ">Sequence_HIJ789\n" + new_sequence_header_HIJ789)
+sequence_header_file.write(">Sequence_DEF456\n" + new_sequence_header_DEF456 + "\n")
+sequence_header_file.write(">Sequence_HIJ789\n" + new_sequence_header_HIJ789)
