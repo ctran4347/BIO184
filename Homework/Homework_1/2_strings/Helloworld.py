a3c90bea92c037ed72417c53b8eb171da2f99a99
xAznboyzx <xAznboyszx@gmail.com>
2018-01-30 21:18:24 -0800

    Fix

---
+++ Homework/Homework_1/2_strings/Helloworld.py
@@ -0,0 +1,14 @@
+print("HELLO\nEARTHLINGS\nWeeeeeeeeeeeeeee")
+# derp
+for my_dna.replace("G","A")
+    pass
+my_dna="ATGCGTA" + "\nGGCC"
+print(my_dna)
+dna_length= len(my_dna)
+print("Derp length  of Derp Sequence is " + str(dna_length))
+print(my_dna.lower())
+print(my_dna.replace("G","A"))
+print(my_dna.replace("C","T"))
+end
+
+
