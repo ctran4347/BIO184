6b754c70037bec995a08f1c60b2e5a5147c97a26
xAznboyzx <xAznboyszx@gmail.com>
2018-02-01 08:26:24 -0800

    Homework 3 Started

---
+++ Homework/Homework_3/tran-christian_processing_dna_in_a_file.py
@@ -0,0 +1,8 @@
+my_file=open("input.txt")
+output=open("output.txt","w")
+for each_line in my_file:
+    length=len(each_line)
+    trimmed=each_line[13:length]
+    output.write(trimmed)
+print("The length of the sequence is " + str(length))
+    
