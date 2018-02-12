ee508f0fd6e6062cdc31c03a9049ea82e9cd55bd
Christian Tran <xAznboyszx@gmail.com>
2018-02-12 08:00:14 -0800

    Midterm Project Update
    Almost done, just need to debug

--- Homework/Midterm Project/Midterm_Project.py
+++ Homework/Midterm Project/Midterm_Project.py
@@ -38,18 +38,58 @@ for lines in sequence_only: # Goes through in each line of file and utilizing so
 first_sequence=sequence_only[0]
 second_sequence=sequence_only[1]
 third_sequence=sequence_only[2]
-
-first_sequence_split=first_sequence.split("CCATGG")
-print(first_sequence_split)
-
-
 # NcoI cleaves at 5' C^CATGG 
 # SacI cleaves at 5' GAGCT^C
-first_sequence_ncoi_count=first_sequence.count("CCATGG")
-second_sequence_ncoi_count=second_sequence.count("CCATGG")
-third_sequence_ncoi_count=third_sequence.count("CCATG")
-print(first_sequence_ncoi_count,second_sequence_ncoi_count,first_sequence_ncoi_count)
-1#NcoI search & split
+# Counts how many restriction sites there are for NcoI, for reference
+ncoi_total=0
+for lines in sequence_only: # Counts the number of NcoI restriction sites
+    ncoi_count=lines.count("CCATGG")
+    ncoi_total=ncoi_total+ncoi_count
+    print("NcoI count is",ncoi_count)
+print("NcoI total is",ncoi_total)
+ncoi_list=[]
+for lines in sequence_only:
+    split=lines.split("CCATGG")
+    split_1=split[0] + str("C")
+    split_2= str("CATGG") + split[1]
+    ncoi_list.append(split_1)
+    ncoi_list.append(split_2)
+print("New NcoI length is",len(ncoi_list),"and list is",ncoi_list)
+saci_total=0
+for lines in ncoi_list: # Counts the number of SacI restriction sties
+    saci_count=lines.count("GAGCTC")
+    saci_total=saci_total+saci_count
+    print("SacI count is",saci_count)
+print("SacI total is",saci_total)
+
+saci_list=[]
+for lines in ncoi_list:
+    split=lines.split("GAGCTC")
+    split_1=split[0]+str("GAGCT")
+    split_2=str("C") + split[1]
+    saci_list.append(split_1)
+    saci_list.append(split_2)
+print("list here",saci_list)
+
+#  sorted_restriction_fragments=sorting_by_length(saci_list)
+# sorted_restriction_fragments_reverse=sorted_restriction_fragments.reverse()
+# for lines in sorted_restriction_fragments:
+#     print("length is",len(lines),lines)
+
+# vector_file=open("pTrc99A.txt").read()
+# vector_ncoi_cut=""
+# for lines in vector_file:
+#     split=lines.split("CCATGG")
+#     split_1=split[0]+largest_fragment
+#     split_final=split_1+split[1]
+#     vector_ncoi_cut=vector_ncoi_cut+split_final
+# final_vector=""
+# for lines in vector_ncoi_cut
+#     split=lines.split("GAGCTC")
+#     split_1=split[0]+largest_fragment
+#     split_final=split_1+split[1]
+#     final_vector=final_vector+split_final
+# print("The final vector file is",final_vector)
 
 
 
