47cd81e89a95d2dc091740a2ba706101a0cbd222
Christian Macbook Pro <ctran@Christians-MacBook-Pro.local>
2018-03-06 13:57:00 -0800

    Midterm Revision and Finalized
    Got each sequence's largest fragment and inserted it into three different vector files.

--- Homework/Midterm_Project/Midterm_Project.py
+++ Homework/Midterm_Project/Midterm_Project.py
@@ -24,7 +24,7 @@ def sorting_gc_content(genomic_sequence):
     length=len(genomic_sequence)
     g_content=genomic_sequence.count("G")
     c_content=genomic_sequence.count("C")
-    gc_content=((g_content+c_content)/(length))
+    gc_content=((g_content+c_content)/(length))*100
     return gc_content
 
 gc_content_list=[]
@@ -44,6 +44,9 @@ for names,gc_content,sequences in sorted(zip(names_only,gc_content_list,sequence
 # NcoI cleaves at 5' C^CATGG 
 # SacI cleaves at 5' GAGCT^C
 
+first_sequence=sequence_only[0]
+second_sequence=sequence_only[1]
+third_sequence=sequence_only[2]
 
 # Counts how many restriction sites there are for NcoI, for reference
 # ncoi_total=0
@@ -53,70 +56,162 @@ for names,gc_content,sequences in sorted(zip(names_only,gc_content_list,sequence
 #     print("NcoI count is",ncoi_count)
 # print("NcoI total is",ncoi_total)
 
-ncoi_list=[]
-for lines in sequence_only: # splits restriction sites and adds nucleotides back
-    if lines.count("CCATGG") != 0:
-        split=lines.split("CCATGG")
-        split_1=split[0] + str("C")
-        split_2= str("CATGG") + split[1]    
-        ncoi_list.append(split_1)
-        ncoi_list.append(split_2)
-    else:
-        pass
-# print(type(sequence_only))
-# print(type(ncoi_list))
-
-#Counts the number of SacI restriction sites for reference
-# saci_total=0
-# for lines in ncoi_list: # Counts the number of SacI restriction sties
-#     saci_count=lines.count("GAGCTC")
-#     saci_total += saci_count
-#     print("SacI count is",saci_count)
-# print("SacI total is",saci_total)
-
-saci_list=[]
-for lines in ncoi_list: # splits restriction sites and adds nucleotides back
-    if lines.count("GAGCTC") !=0:
-        split=lines.split("GAGCTC")
-        split_1=split[0]+str("GAGCT")
-        split_2= str("C") + split[1]
-        saci_list.append(split_1)
-        saci_list.append(split_2)
-    else:
-        pass
-# print(saci_list)
-
 def sorting_by_length(genomic_sequence): # Sorts by length
     #Definition created to sort a genomic sequence by length
     genomic_sequence.sort(key=len)
     return(genomic_sequence)
 
+first_sequence_placeholder_1=[]
+first_sequence_placeholder_2=[]
+ # splits restriction sites and adds nucleotides back
+if first_sequence.count("CCATGG") != 0:
+    split=first_sequence.split("CCATGG")
+    split_1=split[0] + str("C")
+    split_2= str("CATGG") + split[1]    
+    first_sequence_placeholder_1.append(split_1)
+    first_sequence_placeholder_1.append(split_2)
+else:
+    pass
 
+for splices in first_sequence_placeholder_1:
+    if splices.count("GAGCTC") !=0:
+        split=splices.split("GAGCTC")
+        split_1=split[0]+str("GAGCT")
+        split_2= str("C") + split[1]
+        first_sequence_placeholder_2.append(split_1)
+        first_sequence_placeholder_2.append(split_2)
+    else:
+        first_sequence_placeholder_2.append(splices)
 
-sorted_restriction_fragments=sorting_by_length(saci_list) # Sorts fragments
-sorted_restriction_fragments_reverse=sorted_restriction_fragments.reverse() # Reverse the list so that the largest fragment is first
-output3=open("splicings_sorted_by_length.txt","w")
-for lines in sorted_restriction_fragments: # Checks whether the method above works
+first_sequence_sorted=sorting_by_length(first_sequence_placeholder_2) 
+first_sequence_sorted_reverse=first_sequence_sorted.reverse() # Reverse the list so that the largest fragment is first
+
+output3=open("splicings_sorted_by_length_first_sequence.txt","w")
+for lines in first_sequence_sorted: # Checks whether the method above works
     print("Length is" + "\t" + str(len(lines)) + "\t" + lines)
     output3.write("Length is" + "\t" + str(len(lines)) + "\t" + lines + "\n")
 
-largest_fragment=sorted_restriction_fragments[0] # Theoretically largest element should be first
+largest_fragment_first=first_sequence_sorted[0] # Theoretically largest element should be first
 vector=open("pTrc99A.txt").read().replace(">pTrc99A","").upper()
-vector_file=vector[1:].replace("\n","")
+vector_file_1=vector[1:].replace("\n","")
 
-if vector_file.count("CCATGG") != 0:
-    vector_ncoi_cut=vector_file.replace("CCATGG",largest_fragment) # Replaces every NcoI site with the largest fragment
+if vector_file_1.count("CCATGG") != 0:
+    vector_ncoi_cut_1=vector_file_1.replace("CCATGG",largest_fragment_first) # Replaces every NcoI site with the largest fragment
 else:
     print("No NcoI restriction sites")
 
 
-if vector_ncoi_cut.count("GAGCTC") != 0:
-    final_vector=vector_ncoi_cut.replace("GAGCTC",largest_fragment) # Replaces every SacI site with the largest fragment
+if vector_ncoi_cut_1.count("GAGCTC") != 0:
+    final_vector_1=vector_ncoi_cut_1.replace("GAGCTC",largest_fragment_first) # Replaces every SacI site with the largest fragment
 else:
     print("No SocI restriction sites")
 
-print("The Final Vector is",final_vector)
-output4=open("final_vector.txt","w").write(">pTrc99A" + "\n" + final_vector)
+print("The Final Vector is",final_vector_1)
+output4=open("final_vector_1.txt","w").write(">pTrc99A" + "\n" + final_vector_1)
+
+
+ # SECOND SEQUENCE
+second_sequence_placeholder_1=[]
+second_sequence_placeholder_2=[]
+ # splits restriction sites and adds nucleotides back
+if second_sequence.count("CCATGG") != 0:
+    split=second_sequence.split("CCATGG")
+    split_1=split[0] + str("C")
+    split_2= str("CATGG") + split[1]    
+    second_sequence_placeholder_1.append(split_1)
+    second_sequence_placeholder_1.append(split_2)
+else:
+    pass
+
+for splices in second_sequence_placeholder_1:
+    if splices.count("GAGCTC") !=0:
+        split=splices.split("GAGCTC")
+        split_1=split[0]+str("GAGCT")
+        split_2= str("C") + split[1]
+        second_sequence_placeholder_2.append(split_1)
+        second_sequence_placeholder_2.append(split_2)
+    else:
+        second_sequence_placeholder_2.append(splices)
+print(second_sequence_placeholder_2)
+second_sequence_sorted=sorting_by_length(second_sequence_placeholder_2) 
+second_sequence_sorted_reverse=first_sequence_sorted.reverse() # Reverse the list so that the largest fragment is first
+
+output5=open("splicings_sorted_by_length_second_sequence.txt","w")
+for lines in second_sequence_sorted: # Checks whether the method above works
+    print("Length is" + "\t" + str(len(lines)) + "\t" + lines)
+    output3.write("Length is" + "\t" + str(len(lines)) + "\t" + lines + "\n")
+
+largest_fragment_second=second_sequence_sorted[0] # Theoretically largest element should be first
+vector=open("pTrc99A.txt").read().replace(">pTrc99A","").upper()
+vector_file_2=vector[1:].replace("\n","")
+
+if vector_file_2.count("CCATGG") != 0:
+    vector_ncoi_cut_2=vector_file_2.replace("CCATGG",largest_fragment_second) # Replaces every NcoI site with the largest fragment
+else:
+    print("No NcoI restriction sites")
+
+
+if vector_ncoi_cut_2.count("GAGCTC") != 0:
+    final_vector_2=vector_ncoi_cut_2.replace("GAGCTC",largest_fragment_second) # Replaces every SacI site with the largest fragment
+else:
+    print("No SocI restriction sites")
+
+print("The Final Vector is",final_vector_2)
+output6=open("final_vector_2.txt","w").write(">pTrc99A" + "\n" + final_vector_2)
+
+# Third sequence
+third_sequence_placeholder_1=[]
+third_sequence_placeholder_2=[]
+ # splits restriction sites and adds nucleotides back
+if third_sequence.count("CCATGG") != 0:
+    split=third_sequence.split("CCATGG")
+    split_1=split[0] + str("C")
+    split_2= str("CATGG") + split[1]    
+    third_sequence_placeholder_1.append(split_1)
+    third_sequence_placeholder_1.append(split_2)
+else:
+    pass
+
+for splices in third_sequence_placeholder_1:
+    if splices.count("GAGCTC") !=0:
+        split=splices.split("GAGCTC")
+        split_1=split[0]+str("GAGCT")
+        split_2= str("C") + split[1]
+        third_sequence_placeholder_2.append(split_1)
+        third_sequence_placeholder_2.append(split_2)
+    else:
+        third_sequence_placeholder_2.append(splices)
+print(third_sequence_placeholder_2)
+third_sequence_sorted=sorting_by_length(third_sequence_placeholder_2) 
+third_sequence_sorted_reverse=third_sequence_sorted.reverse() # Reverse the list so that the largest fragment is first
+
+output7=open("splicings_sorted_by_length_third_sequence.txt","w")
+for lines in third_sequence_sorted: # Checks whether the method above works
+    print("Length is" + "\t" + str(len(lines)) + "\t" + lines)
+    output3.write("Length is" + "\t" + str(len(lines)) + "\t" + lines + "\n")
+
+largest_fragment_third=third_sequence_sorted[0] # Theoretically largest element should be first
+vector=open("pTrc99A.txt").read().replace(">pTrc99A","").upper()
+vector_file_3=vector[1:].replace("\n","")
+
+if vector_file_3.count("CCATGG") != 0:
+    vector_ncoi_cut_3=vector_file_3.replace("CCATGG",largest_fragment_third) # Replaces every NcoI site with the largest fragment
+else:
+    print("No NcoI restriction sites")
+
+
+if vector_ncoi_cut_3.count("GAGCTC") != 0:
+    final_vector_3=vector_ncoi_cut_3.replace("GAGCTC",largest_fragment_third) # Replaces every SacI site with the largest fragment
+else:
+    print("No SocI restriction sites")
+
+print("The Final Vector is",final_vector_3)
+output8=open("final_vector_3.txt","w").write(">pTrc99A" + "\n" + final_vector_3)
+
+
+
+
+
 
 
 
