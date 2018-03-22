c7732b3362202aa33e1d180aa33d8206b12cc1fb
Christian Macbook Pro <ctran@Christians-MacBook-Pro.local>
2018-03-21 18:30:43 -0700

    Homework 6 complete

---
+++ Homework/Homework_6/dna_translation.py
@@ -0,0 +1,41 @@
+import os
+import re
+def clear():
+    return os.system("clear")
+clear()
+genecode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+temp_list=[]
+
+def translation(sequence):
+    protein = ""
+    last_codon = len(sequence) - 2
+    for start_position in range(0,last_codon,3): # Iterates starting at 0 and increases by 3 indexes each time
+         current_codon = sequence[start_position:start_position + 3]
+         amino_acid=genecode.get(current_codon,"X")
+         protein += amino_acid
+    return protein
+
+         
+assert(translation("ATGTTCGGT")) == "MFG"
+assert(translation("ATCGATCGAT")) == "IDR"
+assert(translation("ACGANCGAT")) == "TXD" 
+
+
+
+    
