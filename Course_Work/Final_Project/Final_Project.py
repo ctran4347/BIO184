fbd6685de965ef594747392de4acdbf242cabe8b
Christian Tran <xAznboyszx@gmail.com>
2018-05-23 21:06:24 -0700

    Final

--- Course_Work/Final_Project/Final_Project.py
+++ Course_Work/Final_Project/Final_Project.py
@@ -1,7 +1,10 @@
-import os,re, sys
+import os
+import re
+import sys
+
 #---------------------------------
 #Clears screen
-os.system('clear')
+os.system('cls')
 #---------------------------------
 genecode = {
     'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
@@ -33,8 +36,10 @@ def translation(sequence):
 #-------------------------------------
 class DNARecord(object):
     def __init__(self,file_name):
+        #constructor function
         self.file_name=file_name    
     def species_name(self):
+        #function that seperates species from file 
         species_list=[]
         for line in open(self.file_name):
             if line.startswith(">"):
@@ -47,6 +52,7 @@ class DNARecord(object):
                 pass
         return species_list
     def group_number(self):
+        # function that seperates group number from file
         group_list=[]
         for line in open(self.file_name):
             if line.startswith(">"):
@@ -56,19 +62,22 @@ class DNARecord(object):
                 pass
         return group_list
     def sequence(self):
+        # function that seperates sequence from file
         sequence_list=[]
-        temporary_sequence_string = ""
+        temporary_sequence_string=""
         for line in open(self.file_name):
-            if line.startswith(">"):
-                sequence_list.append(temporary_sequence_string)
-                temporary_sequence_string = ""
-            else:
+            if not line.startswith(">"):
                 temporary_sequence_string += line.strip()
+            elif line.startswith(">"):
+                if sequence_list == []:
+                    pass
+                sequence_list.append(temporary_sequence_string)        
+                temporary_sequence_string=""
         return sequence_list
 #--------------------------------------
-d1=DNARecord("multi-species_alignments.fa")
-print(d1.sequence())
+d1=DNARecord(sys.argv[1])
 def compare(str1,str2):
+    # Function to compare two strings
     if len(str1) == len(str2):
         max_length=len(str1)
         letters_match=0
@@ -95,7 +104,6 @@ def compare(str1,str2):
                 pass
     return letters_match / max_length *100
 #---------------------------------------
-
 test_dict={}
 temp_name_list=[]
 temp_group_list=[]
@@ -107,20 +115,26 @@ human_group_number=""
 human_sequence=""
 test_list=[]
 for name,group,sequence in zip(d1.species_name(),d1.group_number(),d1.sequence()): 
+    #Iterates through list of tuples 
     if name.startswith(">Hs"):
+        #Checks if tuple starts with >H and sets human variables accordingly
         human_name=name
         human_group_number=group
         human_sequence=sequence
         test_list.append(human_sequence)
     else: 
+        #Sets other genes accordingly
         temp_name_list.append(name) # species name
         temp_group_list.append(group) # group number
         temp_sequence_list.append(sequence) # sequence
     if group not in test_dict.keys():
+        #Checks whether group has been inputed to dictionary counter. If not creates a new key and value pair
         test_dict[group]= 1
     elif group in test_dict.keys():
+        #Checks if group exists in dictionary. If it does, updates value
         test_dict[group] += 1
     if test_dict.get(group) == 5:
+        #Checks if value for key is 5 and compares human variables to other gene variables
         for name,sequeunce in zip(temp_name_list,temp_sequence_list):
             translated_human_sequence=translation(human_sequence)
             translated_sequeunce=translation(sequence)
@@ -130,16 +144,20 @@ for name,group,sequence in zip(d1.species_name(),d1.group_number(),d1.sequence()
                 similarity_dict[name].append(similarity)
             elif name in similarity_dict.keys(): # its 96 times
                 similarity_dict[name].append(similarity)
+        # Resets lists and variables
         temp_name_list=[]
         temp_group_list=[]
         temp_sequence_list=[]
         human_name=""
         human_group_number=""
-        human_sequence=""
+        human_sequence=""    
     else:
+    # If value for key is not 5. It would continue
         pass
-
-# -------------------------------------
-# for name,similarity in similarity_dict.items():
-#     true_average= sum(similarity) / float(len(similarity))
-#     print(name,"\n",true_average)
+#-------------------------------------
+#Prints out average of similarities for each gene.
+for name,similarity in similarity_dict.items():
+    true_average= sum(similarity) / float(len(similarity))
+    print(name,"\n",true_average)
+#-------------------------------------
+# python3 Final_Project.py multi-species_alignments.fa
